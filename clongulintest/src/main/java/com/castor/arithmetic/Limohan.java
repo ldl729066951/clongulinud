605985138db668cd6e15efae505373a7f7d52420
ldl729066951 <729066951@qq.com>
2018-10-24 18:40:44 +0800

    add arithmetic to limohan

--- clongulintest/src/main/java/com/castor/arithmetic/Limohan.java
+++ clongulintest/src/main/java/com/castor/arithmetic/Limohan.java
@@ -2,6 +2,16 @@ package com.castor.arithmetic;
 
 public class Limohan {
 
+	public static void main(String[] args) {
+		double[] arg = new double[]{1.2, 1.3};
+		double[] res = test(arg);		
+		
+	}
 
+	private static double[] test(double[] arg) {
+
+
+		return null;
+	}
 
 }
